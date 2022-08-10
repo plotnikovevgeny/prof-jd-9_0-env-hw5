da1e9cdea0b984bb6d9599d798bb028e0aea2b64
Plotnikov Evgeny <e.plotnikov@vk.team>
2022-08-10 14:22:55 +0300

    HW Done

--- src/main/java/pro/sky/Main.java
+++ src/main/java/pro/sky/Main.java
@@ -2,6 +2,7 @@ package pro.sky;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        boolean check = Check.checkCred("java_skyprogo", "D_1hWiKjjP_9", "D_1hWiKjjP_9");
+        System.out.println(check);
     }
 }
